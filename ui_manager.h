5756e78ccd7339c96af9aa6bcc7e645eaefd7eec
fredrikrudin <49065026+fredrikrudin@users.noreply.github.com>
2026-08-26 16:20:06 +0200

    Update ui_manager.h

--- ui_manager.h
+++ ui_manager.h
@@ -100,4 +100,30 @@ inline void add_manual_relay_to_settings(lv_obj_t * list, uint8_t relayPin, cons
     lv_obj_add_event_cb(sw, ui_manual_relay_event_handler, LV_EVENT_VALUE_CHANGED, (void*)(uint32_t)relayPin);
 }
 
+// --- Globala UI-pekare för mätare (Victron GUI-v2) ---
+extern lv_obj_t *battery_arc;
+extern lv_obj_t *lbl_soc_value;
+extern lv_obj_t *lbl_power_value;
+extern lv_obj_t *lbl_specs_value;
+extern lv_obj_t *mppt_arc;
+extern lv_obj_t *lbl_mppt_pwr;
+extern lv_obj_t *ip22_arc;
+extern lv_obj_t *lbl_ip22_pwr;
+extern lv_obj_t *lbl_inverter_pwr;
+extern lv_obj_t *lbl_ruuvi_data;
+extern lv_obj_t *lbl_xiaomi_data;
+
+// --- Globala UI-objekt för animationer ---
+extern lv_obj_t *particle_mppt;
+extern lv_obj_t *particle_ip22;
+extern lv_obj_t *particle_inverter;
+extern lv_anim_t anim_mppt;
+extern lv_anim_t anim_ip22;
+extern lv_anim_t anim_inverter;
+
+// --- Funktionsdeklarationer ---
+void create_overview_page(lv_obj_t *parent);
+void ui_update_live_data();
+
+
 #endif // UI_MANAGER_H
